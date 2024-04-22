95574f716e9dfcc7c8796812cb8efe64e0e9bad0
BenBasilTomy <95748096+BenBasilTomy@users.noreply.github.com>
2024-04-22 15:33:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const port = 8443;
-var key = fs.readFileSync(__dirname + '/selfsigned.key');
-var cert = fs.readFileSync(__dirname + '/selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert
-};
+
 app = express()
 app.get('/', (req, res) => {
   res.status(200).json({'message':'Hello World'});
@@ -16,7 +11,7 @@ app.post('/testing', (req, res) =>{
   console.log(req)
   res.status(200).json({'message':'done'})
 })
-var server = https.createServer(options, app);
-server.listen(port, () => {
+
+app.listen(port, () => {
   console.log("server starting on port : " + port)
 });
