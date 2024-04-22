1b1e5025b4358109db6c7e550812feb2a8373f1a
BenBasilTomy <95748096+BenBasilTomy@users.noreply.github.com>
2024-04-22 17:06:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.status(200).json({'message':'Hello World'});
 })
 app.post('/testing', (req, res) =>{
-  console.log(req)
+  console.log("REQUEST BODY !!!!!!!!!!!!!!!!!!!!!!!",req.body)
   res.status(200).json({'message':'done'})
 })
 
