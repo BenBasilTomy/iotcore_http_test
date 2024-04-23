1fcea008cf5fcb872e782b85694bcb81eca9752a
BenBasilTomy <95748096+BenBasilTomy@users.noreply.github.com>
2024-04-23 10:47:16 +0530

    fix:Add body parser in index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const port = 8443;
-
-app = express()
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
-  res.status(200).json({'message':'Hello World'});
+    res.status(200).json({'message':'Hello World'});
 })
 app.post('/testing', (req, res) =>{
-  console.log("REQUEST BODY !!!!!!!!!!!!!!!!!!!!!!!",req.body)
-  res.status(200).json({'message':'done'})
+    console.log(req)
+    res.status(200).json({'message':'done'})
 })
-
-app.listen(port, () => {
-  console.log("server starting on port : " + port)
-});
+app.listen(8443, () => {
+    console.log('server running 8443')
+})
