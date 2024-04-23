bdbe8d5532efb2bbdb388813a507ff8388b3e2de
BenBasilTomy <95748096+BenBasilTomy@users.noreply.github.com>
2024-04-23 10:54:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.status(200).json({'message':'Hello World'});
 })
 app.post('/testing', (req, res) =>{
-    console.log(req)
+    console.log("BODY!!",req.body)
     res.status(200).json({'message':'done'})
 })
 app.listen(8443, () => {
